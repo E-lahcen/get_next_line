2619861c32f1ca28e1409c4944a1858582220446
E-lahcen <lahcenelhlami@gmail.com>
2021-12-11 17:21:11 +0100

    update get-next-line

--- get_next_line.c
+++ get_next_line.c
@@ -5,28 +5,67 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lelhlami <lelhlami@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/09 15:35:57 by lelhlami          #+#    #+#             */
-/*   Updated: 2021/12/10 18:35:51 by lelhlami         ###   ########.fr       */
+/*   Created: 2021/12/11 12:22:52 by lelhlami          #+#    #+#             */
+/*   Updated: 2021/12/11 17:20:39 by lelhlami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_getline(char *save)
+{
+    char    *s;
+    int     i;
+
+    i = 0;
+    if (!save[i])
+        return (NULL);
+    while (save[i] && save[i] != '\n')
+        i++;
+    s = ft_substr(save, 0, i + 2);
+    if (!s)
+        return (NULL);
+    free (save);    //free first version of save
+    if (save[i] == '\n')
+        save = ft_strdup(s + i + 1); //initialise save with the next lines
+    if (!save[i])
+        save = NULL;
+    return (s);
+}
+
 char	*get_next_line(int fd)
 {
-	int red;
-	char *buf;
-	static char *save;
-
-	buf = (char *)malloc(BUFFER_SIZE + 1);
-	while (red = read(fd, buf, BUFFER_SIZE))
-	{
-		if (ft_strchr(buf, '\n'))
-			break;
-		buf[red] = '\0';
-		save = ft_strjoin(save, buf);
-	}
-	return (save);
+    char            *buf;
+    char            *line;
+    static char     *save = NULL;
+    int             rd;
+    
+    printf("here");
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    rd = 1;
+    buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+    if (!buf)
+        return (NULL);
+    while (!ft_strchr(save, '\n') && rd)
+    {
+        rd = read(fd, buf, BUFFER_SIZE);
+        if (rd <= 0)
+        {
+            free(buf);
+            return (NULL);
+        }
+        buf[BUFFER_SIZE] = '\0';
+        save = ft_strjoin(save, buf);
+        if (!save) //if the copy didn't happened return NULL
+        {
+            free(save);
+            return (NULL);
+        }
+    }
+    free(buf); //on vide buffer
+    line = ft_getline(save);
+    return (line);
 }
 
 int	main(void)
@@ -38,9 +77,9 @@ int	main(void)
 	{
 		line = get_next_line(fd);
 		printf("%s", line);
-		// free(line);
 		i++;
 	}
+	free(line);
 	//printf("|\n");
 	//system("leaks a.out");
     return (0);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lelhlami <lelhlami@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 15:37:27 by lelhlami          #+#    #+#             */
-/*   Updated: 2021/12/10 15:14:08 by lelhlami         ###   ########.fr       */
+/*   Updated: 2021/12/11 14:46:52 by lelhlami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,6 @@ size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void	*ft_calloc(size_t count, size_t size);
-char	**ft_split(char const *s, char c);
+char	*ft_strchr(const char *s, int c);
 void	ft_bzero(void *s, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
